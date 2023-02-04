e87cf7cdbe547e3240321ace169114c4195a16df
Alejandro Bolivar <abolivar@abolivar-ltm1.internal.salesforce.com>
2023-02-04 08:31:06 -0500

    Added more files

---
+++ myenv/exercise05.py
@@ -0,0 +1,15 @@
+number = int(input("Enter a number: "))
+
+
+while number !=0:
+
+    if number  == 0:
+        break
+    elif number %2 == 0:
+        print("Even")
+        break
+    else:
+        print ("Odd")
+        break
+print("Goodbye")
+    
