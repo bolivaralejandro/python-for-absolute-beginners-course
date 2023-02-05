5d3830250db6eddcc145a82856eebdb95ddec732
Alejandro Bolivar <bolivaral@gmail.com>
2023-02-05 16:18:04 -0500

    added more rps code

--- myenv/m-n-m/rpsgame.py
+++ myenv/m-n-m/rpsgame.py
@@ -8,10 +8,12 @@ player_2 = input("Enter player 2's name ")
 rolls = ['rock', 'paper', 'scissors']
 
 roll1 = input(f"{player_1}, what is your roll? [rock, paper, scissors]: ")
+roll1 = roll1.lower().strip()
 if roll1 not in rolls:
     print(f"Sorry {player_1}, {roll1} is not an valid play!")
 
 roll2 = input(f"{player_2}, what is your roll? [rock, paper, scissors]: ")
+roll2 = roll2.lower().strip()
 if roll2 not in rolls:
     print(f"Sorry {player_2}, {roll2} is not an valid play!")
 
@@ -21,6 +23,31 @@ print(f"{player_2} rolls {roll2}")
 
 # Test for a winner
 winner = None
+
+if roll1 == roll2:
+    print("The play was tied!")
+elif roll1 == 'rock':
+    if roll2 == 'paper':
+        winner = player_2
+    elif roll2 == 'scicssors':
+        winner = player_1
+elif roll1 == 'paper':
+    if roll2 == 'scissors':
+        winner = player_2
+    elif roll2 == 'rock':
+        winner = player_1
+elif roll1 == 'scissors':
+    if roll2 == 'rock':
+        winner = player_2
+    elif roll2 == 'paper':
+        winner = player_1
+
+print("The game is over")
+if winner is None:
+    print("It was a tie!")
+else:
+    print(f"{winner} takes the game!") 
+
 # Rock
 #     Rock -> tie
 #     Paper -> lose
