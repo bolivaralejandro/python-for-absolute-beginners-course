e443fda730ddc9c91abb1998ae75b4756af1c9d8
Alejandro Bolivar <bolivaral@gmail.com>
2023-02-05 16:27:44 -0500

    added more rps code

--- myenv/m-n-m/rpsgame.py
+++ myenv/m-n-m/rpsgame.py
@@ -1,35 +1,25 @@
+import random
+
 print("---------------------------")
 print(" Rock Paper Scissors v1")    
 print("---------------------------")
 
-player_1 = input("Enter player 1's name ")
-player_2 = input("Enter player 2's name ")
+player_1 = "You"
+player_2 = "Computer"
 
 rolls = ['rock', 'paper', 'scissors']
 
 roll1 = input(f"{player_1}, what is your roll? [rock, paper, scissors]: ")
-<<<<<<< HEAD
 roll1 = roll1.lower().strip()
-=======
->>>>>>> 11d6344a2ae1ffb4880ebe5d490cc8af816f6a69
 if roll1 not in rolls:
     print(f"Sorry {player_1}, {roll1} is not an valid play!")
 
-roll2 = input(f"{player_2}, what is your roll? [rock, paper, scissors]: ")
-<<<<<<< HEAD
-roll2 = roll2.lower().strip()
-=======
->>>>>>> 11d6344a2ae1ffb4880ebe5d490cc8af816f6a69
-if roll2 not in rolls:
-    print(f"Sorry {player_2}, {roll2} is not an valid play!")
+roll2 = random.choice (rolls)
 
-
-print(f"{player_1} rolls {roll1}")
+print(f"{player_1} roll {roll1}")
 print(f"{player_2} rolls {roll2}")
 
-# Test for a winner
 winner = None
-<<<<<<< HEAD
 
 if roll1 == roll2:
     print("The play was tied!")
@@ -55,8 +45,7 @@ if winner is None:
 else:
     print(f"{winner} takes the game!") 
 
-=======
->>>>>>> 11d6344a2ae1ffb4880ebe5d490cc8af816f6a69
+# Test for a winner
 # Rock
 #     Rock -> tie
 #     Paper -> lose
