7dad7a2ac55ec6f4b143a55aaf3be62b3c1b7bb1
Alejandro Bolivar <bolivaral@gmail.com>
2023-02-07 08:46:09 -0500

    more functions

--- myenv/m-n-m/rpsgame.py
+++ myenv/m-n-m/rpsgame.py
@@ -88,12 +88,21 @@ def check_for_winning_throw(player_1, player_2, roll1, roll2):
 
 
 def get_roll(player_name, rolls):
-    roll = input(f"{player_name}, what is your roll? [rock, paper, scissors]: ")
-    roll = roll.lower().strip()
-    if roll not in rolls:
+    print("Available rolls:")
+    for index, r in enumerate(rolls, start=1):
+        print(f"{index}. {r}")
+
+    
+    text = int(input(f"{player_name}, what is your roll? "))
+    selected_index = int(text) - 1
+
+    
+    if selected_index < 0 or selected_index >= len(rolls):
+        print(f"Sorry {player_name}, {text} is not a valid play!")
         return None
 
-    return roll
+    return rolls[selected_index]
+
 
 if __name__ == '__main__':
     main()
